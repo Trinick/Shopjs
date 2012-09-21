0fc0437539e5cdb18f2cddb124994dde142b2ec9
root <root@li224-139.members.linode.com>
2012-09-21 05:14:59 -0400

    Template commit

--- shop.js
+++ shop.js
@@ -3,5 +3,9 @@ var app = express();
 app.get('/', function(req, res) {
 	res.send('This software is currently in development.');
 });
+app.use(function(err, req, res, next) {
+	console.error(err.stack);
+	res.send(500, 'Something went wrong with your request. Please refresh the page or go back to your previous browsing page.');
+});
 app.listen(80);
 console.log('Listening on port 80');
