b6c80a236440fe8dda3aaa0ff3dae3d1e26447b2
Billy Fallon <slicksilver555@mac.com>
2012-10-05 23:19:24 -0700

    update

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Shopjs', url: req.url });
+  res.render('index', { title: 'Shopjs', url: req.url, req: req, res: res });
 };
