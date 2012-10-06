e7022052b3e11ad6c87b21bf3e36675afeff7d4f
Billy Fallon <slicksilver555@mac.com>
2012-10-05 23:39:22 -0700

    update

--- shop.js
+++ shop.js
@@ -1,11 +1,36 @@
-var express = require('express');
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
 var app = express();
-app.get('/', function(req, res) {
-	res.send('This software is currently in development.');
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'public')));
 });
-app.use(function(err, req, res, next) {
-	console.error(err.stack);
-	res.send(500, 'Something went wrong with your request. Please refresh the page or go back to your previous browsing page.');
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
 });
-app.listen(80);
-console.log('Listening on port 80');
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
