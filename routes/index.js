a077fbe1356643b4fb8404e0f3016cbf465d315d
Billy Fallon <slicksilver555@mac.com>
2012-10-07 00:18:50 -0700

    update

--- routes/index.js
+++ routes/index.js
@@ -13,3 +13,14 @@ exports.index = function(req, res){
   , footertext: 'Copyright 2012 Shopjs'
    });
 };
+
+exports.filter = function(req, res){
+  var filters = req.url.split("/");
+  res.render('index', { title: 'Shopjs'
+  , url: req.url
+  , req: req
+  , res: res
+  , pagecontent: filters
+  , footertext: 'Copyright 2012 Shopjs'
+   });
+};
--- shop.js
+++ shop.js
@@ -28,7 +28,8 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/*', routes.index);
+app.get('/', routes.index)
+app.get('/*', routes.filter);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
