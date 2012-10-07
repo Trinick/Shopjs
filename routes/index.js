72d30d983f0f59a0c0767ffadd066ba0547c3085
Billy Fallon <slicksilver555@mac.com>
2012-10-06 23:56:48 -0700

    update

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,12 @@
  */
 
 exports.index = function(req, res){
+  var filters = req.url.split("/");
   res.render('index', { title: 'Shopjs'
   , url: req.url
   , req: req
   , res: res
+  , content: filters
   , footertext: 'Copyright 2012 Shopjs'
-  , pagecontent: 'WIP YO'
    });
 };
